44e129ce606133d41f4f62beb3a061e909814998
deeksha verma <deekshav017@gmail.com>
2019-03-25 17:28:09 +0530

    add index.ejs for rendering

--- app.js
+++ app.js
@@ -7,14 +7,25 @@ const myDB = 'mongodb://localhost/nodejs';
 const User = ('./Models/users/model.js');
 const UserRoutes = require('./Controllers/userroutes');
 const bodyParser = require('body-parser');
+const ejs = require('ejs');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
 mongoose.connect(myDB);
 app.use('/user', UserRoutes);
+
+app.get('/', (req, res) => {
+  let user = {
+    name: 'joe',
+  }
+  res.render('index',{ user: user });
+});
 
 app.listen(port, (err) => {
   if (!err) {
