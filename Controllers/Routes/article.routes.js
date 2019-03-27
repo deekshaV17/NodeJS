13ca29f5eab25e5881ea24c536d39a68edfdbec9
deeksha verma <deekshav017@gmail.com>
2019-03-27 11:06:24 +0530

    add dashboard view

--- Controllers/Routes/article.routes.js
+++ Controllers/Routes/article.routes.js
@@ -1,3 +1,25 @@
-const Router = require('express').Router();
+const Router = require('express').Router(),
+  Article = require('../../Models/article.model');
+
+Router.post('/new', (req, res) => {
+  if(req.session.user || req.user) {
+    let user = req.session.user || req.user;
+    let newArticle = new Article;
+    newArticle.title = req.body.title;
+    newArticle.description = req.body.description;
+    newArticle.image = req.body.image;
+    newArticle.tags = req.body.tags;
+    newArticle.author = user._id;
+    newArticle.save((err, article) => {
+      if (err) {
+        console.log('error creating article', err);
+      } else {
+        res.redirect('/dashboard');
+      }
+    });
+  } else {
+    res.redirect('/');
+  }
+});
 
 module.exports = Router;
--- Controllers/Routes/base.routes.js
+++ Controllers/Routes/base.routes.js
@@ -1,4 +1,6 @@
-const Router = require('express').Router();
+const Router = require('express').Router(),
+  Article = require('../../Models/article.model'),
+  User = require('../../Models/user.model');
 
 Router.get('/', (req, res) => {
   if(!req.session.user) {
@@ -12,6 +14,39 @@ Router.get('/login', (req, res) => {
 
 Router.get('/signup', (req, res) => {
   res.render('signup', { errors: [] });
+});
+
+Router.get('/new_article', (req, res) => {
+  res.render('new_article');
+});
+
+Router.get('/search', (req, res) => {
+  res.render('search', { articles: [] });
+});
+
+Router.get('/search/:tag', (req, res) => {
+  Article.find({ tags: req.params.tag }).populate('author', 'username').then((articles) => {
+    res.render('search', { articles, });
+  });
+});
+
+Router.get('/dashboard', (req, res) => {
+  let errors = [];
+  if (req.session.user || req.user) {
+    let user = req.session.user || req.user;
+    let user_ids = [];
+    User.find({ username: { $in: user.following } }).then((users) => {
+      users.forEach((user) => user_ids.push(user._id));
+    }).then(() => {
+      Article.find({ author: { $in: user_ids }}).populate('author', ['username']).then((articles) => {
+        res.render('dashboard', { articles, });
+      });
+    }).catch((err) => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 module.exports = Router;
